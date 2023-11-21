ae82c1267f9ee9bfa8f5133146d31d9ccf69fe8b
gkris <gkris@192.168.0.183>
2023-11-21 12:46:33 +0100

    Third commit

--- dummy/src/dummy/github2.java
+++ dummy/src/dummy/github2.java
@@ -1,9 +1,9 @@
 package dummy;
 
-public class github {
+public class github2 {
 public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("Hello all");
-	System.out.println("HI");
+	System.out.println("Hello all welcome");
 }
 }
